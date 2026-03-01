d44efd5fa411d03acec8e27984f95e5675242b99
toni <canillask8@gmail.com>
2026-03-01 20:03:34 +0100

    feat: add ProtectedRoute, connect router to main and fix env path

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen bg-black text-white">
-      <h1 className="text-green-500 text-4xl">Waz Here 🚩</h1>
-    </div>
-  )
-}
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import AppRoutes from './router/app.routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <AppRoutes />
+  </StrictMode>
 )
